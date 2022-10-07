791b1d0dbff34e7f38eda0491961e0cb0408c25b
Sion Shin <82511301+Sion99@users.noreply.github.com>
2022-10-07 21:32:31 +0900

    Update lab5_2018112085.sql

--- Database/lab5_2018112085.sql
+++ Database/lab5_2018112085.sql
@@ -1,6 +1,6 @@
 --Lab#5-1 & Lab #5-2
-grant all privileges to university;
-conn university/comp322
+SQL> grant all privileges to university;
+SQL> conn university/comp322
 
 DROP TABLE DEPARTMENT CASCADE CONSTRAINT;
 CREATE TABLE DEPARTMENT(
@@ -85,4 +85,37 @@ insert into dept_locations values (4, 'Stafford');
 insert into dept_locations values (5, 'Bellaire');
 insert into dept_locations values (5, 'Sugarland');
 insert into dept_locations values (5, 'Houston');
+
+insert into DEPENDENT values ('333445555', 'Alice', 'F', TO_DATE('1986-04-05', 'yyyy-mm-dd'), 'Daughter');
+insert into DEPENDENT values ('333445555', 'Theodore', 'M', TO_DATE('1983-10-25', 'yyyy-mm-dd'), 'Son');
+insert into DEPENDENT values ('333445555', 'Joy', 'F', TO_DATE('1958-05-03', 'yyyy-mm-dd'), 'Spouse');
+insert into DEPENDENT values ('987654321', 'Abner', 'M', TO_DATE('1942-02-28', 'yyyy-mm-dd'), 'Spouse');
+insert into DEPENDENT values ('123456789', 'Michael', 'M', TO_DATE('1988-01-04', 'yyyy-mm-dd'), 'Son');
+insert into DEPENDENT values ('123456789', 'Alice', 'F', TO_DATE('1988-12-30', 'yyyy-mm-dd'), 'Daughter');
+insert into DEPENDENT values ('123456789', 'Elizabeth', 'F', TO_DATE('1967-05-05', 'yyyy-mm-dd'), 'Spouse');
+
+insert into PROJECT values ('ProductX', 1, 'Bellaire', 5);
+insert into PROJECT values ('ProductY', 2, 'Sugarland', 5);
+insert into PROJECT values ('ProductZ', 3, 'Houston', 5);
+insert into PROJECT values ('Computerization', 10, 'Stafford', 4);
+insert into PROJECT values ('Reorganization', 20, 'Houston', 1);
+insert into PROJECT values ('Newbenefits', 30, 'Stafford', 4);
+
+insert into WORKS_ON values ('123456789', 1, 32.5);
+insert into WORKS_ON values ('123456789', 2, 7.5);
+insert into WORKS_ON values ('666884444', 3, 40.0);
+insert into works_on values ('453453453', 1, 20.0);
+insert into works_on values ('453453453', 2, 20.0);
+insert into works_on values ('333445555', 2, 10.0);
+insert into works_on values ('333445555', 3, 10.0);
+insert into works_on values ('333445555', 10, 10.0);
+insert into works_on values ('333445555', 20, 10.0);
+insert into works_on values ('999887777', 30, 30.0);
+insert into works_on values ('999887777', 10, 10.0);
+insert into works_on values ('987987987', 10, 35.0);
+insert into works_on values ('987987987', 30, 5.0);
+insert into works_on values ('987654321', 30, 20.0);
+insert into works_on values ('987654321', 20, 15.0);
+insert into works_on values ('888665555', 20, NULL);
+
 
