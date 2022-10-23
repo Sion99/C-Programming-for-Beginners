eae01a7421478617ab14564ab94a81b705f96a0a
sion99 <kmicety1@gmail.com>
2022-10-23 15:57:19 +0900

    fixed point

---
+++ fixedpoint.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def g(x):
+    return np.exp(-x)
+
+x = 0
+es = 10**-6
+count = 0
+while(True):
+    count += 1
+    temp = g(x)
+    x = temp
+    print(f'{count}회차 : x = {x} g(x) = {g(x)}')
+    if abs((g(x) - x)/g(x)) < es:
+        break
